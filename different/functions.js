17474f67527aa4e1660f6bcd1cc0a6edc961b339
Yuliia <alice1yulia@gmail.com>
2023-09-22 22:45:52 +0200

    tried filter function

--- different/functions.js
+++ different/functions.js
@@ -51,3 +51,26 @@ sayHello2('Chesc')
 const sayHaw = pug.voice();
 pug.voice();
 'Haw haw haw'
+
+
+// filter higher order functions:
+
+//завдання: віфільтрувати масив, знайти значення start > 2000, і < 2000,результат в консоль.
+const companies = [
+  {name: "Companie One", category: "Finance", start: 1984, end: 2001},
+  {name: "Companie Two", category: "Retail", start: 2003, end: 2020},
+  {name: "Companie Three", category: "Auto", start: 1999, end: 2015},
+  {name: "Companie Seven", category: "Science", start: 1999, end: 2006}
+];
+    
+
+const filteredArray = companies.filter(function (item) {
+  return item.start < 2000;
+});
+console.log(filteredArray);
+
+const filteredArray = companies.filter(function(item){
+  return item.start > 2000;
+});
+
+console.log(filteredArray);
