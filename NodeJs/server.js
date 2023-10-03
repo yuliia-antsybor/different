38e7c413de64b60ab06052144d6a4a84c673a820
Yuliia <alice1yulia@gmail.com>
2023-10-03 21:44:49 +0200

    try server methods

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,9 +1,16 @@
 //мудулі, які нам щось повертають, наприклад 'os', 'http', 'path' 'fs'...etc
-const os =require('os'); //повертає нам об'єкт модуля
+const os = require('os'); //повертає нам об'єкт модуля
+const fs = require('fs');
 
 // let = 6;
 // a += 6;
 // console.log("value =", a);
 
-console.log(os.type());
-console.log(os.arch());
+// console.log(os.type());
+// console.log(os.arch());
+
+//запишем дані в файл
+fs.writeFile('my_file.txt',"Hello there", function(err) {
+  if(err)console.log(err.message);
+  console.log('Saved!');
+})
