837b94f37bb6a89084c54f98d87b35e9395f949e
Yuliia <alice1yulia@gmail.com>
2023-10-03 19:36:32 +0200

    server training

---
+++ NodeJs/server.js
@@ -0,0 +1,9 @@
+//мудулі, які нам щось повертають, наприклад 'os', 'http', 'path' 'fs'...etc
+const os =require('os'); //повертає нам об'єкт модуля
+
+// let = 6;
+// a += 6;
+// console.log("value =", a);
+
+console.log(os.type());
+console.log(os.arch());
